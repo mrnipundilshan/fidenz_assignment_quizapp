6d64e30a3541558705c7197ca4fde9f3f0730bce
mrnipundilshan <nipundilshan991@gmail.com>
2025-10-30 00:04:49 +0530

    initialize env in app

--- lib/data/services/quection_api_service.dart
+++ lib/data/services/quection_api_service.dart
@@ -8,7 +8,7 @@ class QuectionApiService {
       baseUrl: dotenv.env['API_BASE_URL'] ?? '',
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
-      headers: {'content-Type': 'application/json'},
+      headers: {'Content-Type': 'application/json'},
     ),
   );
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:fidenz_assignment_quizapp/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // load env variables
+  await dotenv.load(fileName: ".env");
+  print("API_BASE_URL = ${dotenv.env['API_BASE_URL']}");
+
   runApp(const MainApp());
 }
