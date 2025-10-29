b1a51bc4884708006c91b500dbbb06040045a4b7
mrnipundilshan <nipundilshan991@gmail.com>
2025-10-29 21:25:02 +0530

    make keypad button

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatefulWidget {
-  const MyButton({super.key});
+  final double width;
+  final String title;
+  final bool isSelected;
+  final VoidCallback? function;
+  const MyButton({
+    super.key,
+    required this.width,
+    required this.title,
+    required this.isSelected,
+    this.function,
+  });
 
   @override
   State<MyButton> createState() => _MyButtonState();
@@ -10,6 +20,26 @@ class MyButton extends StatefulWidget {
 class _MyButtonState extends State<MyButton> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: widget.width * 0.2,
+      height: widget.width * 0.2,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(0xFF6e377e),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15), // circular corners
+          ),
+        ),
+        child: Text(
+          "1",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: widget.width * 0.1,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/features/home_page.dart
+++ lib/features/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:fidenz_assignment_quizapp/components/my_button.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(backgroundColor: Color(0xFFb596b9));
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Color(0xFFb596b9),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [MyButton(width: width, title: '1', isSelected: true)],
+        ),
+      ),
+    );
   }
 }
