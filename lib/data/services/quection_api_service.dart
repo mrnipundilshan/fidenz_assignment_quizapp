e5576e61a2ad30b2d896f8dfb8bc90e04d49097a
mrnipundilshan <nipundilshan991@gmail.com>
2025-10-29 23:10:22 +0530

    get quection api function

--- lib/data/services/quection_api_service.dart
+++ lib/data/services/quection_api_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:fidenz_assignment_quizapp/models/quection_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class QuectionApiService {
@@ -10,4 +11,21 @@ class QuectionApiService {
       headers: {'content-Type': 'application/json'},
     ),
   );
+
+  // get quections
+  Future<QuectionModel> getQections() async {
+    try {
+      final response = await _dio.get('');
+
+      if (response.statusCode == 200) {
+        return QuectionModel.fromJson(response.data);
+      } else {
+        throw Exception(
+          "Failed to fetch quections, status: ${response.statusCode}",
+        );
+      }
+    } catch (e) {
+      throw Exception("Something went wrong: $e");
+    }
+  }
 }
