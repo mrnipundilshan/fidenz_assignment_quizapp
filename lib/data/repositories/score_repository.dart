9651e67aa343b823b461ef0a77945c0272b0e999
mrnipundilshan <nipundilshan991@gmail.com>
2025-10-30 18:56:03 +0530

    shared pref repo

--- lib/data/repositories/score_repository.dart
+++ lib/data/repositories/score_repository.dart
@@ -1,1 +1,16 @@
+import 'package:fidenz_assignment_quizapp/data/services/local_storage_service.dart';
 
+class ScoreRepository {
+  final LocalStorageService _service = LocalStorageService();
+
+  // get high score
+  Future<int> getHighScore() => _service.getHighScore();
+
+  // save high score
+  Future<void> saveHighScore(int newScore) async {
+    final currentHighScore = await _service.getHighScore();
+    if (newScore > currentHighScore) {
+      await _service.saveHighScore(newScore);
+    }
+  }
+}
